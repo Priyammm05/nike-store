6d1befe07ab7316978e0b52bff260a9e14fff3b6
Priyam <priyam05soni@gmail.com>
2023-01-06 13:49:02 +0530

    updated sales components

--- src/components/Sales.jsx
+++ src/components/Sales.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Item from "./utils/Item";
-import Title from "./utils/title";
+import Title from "./utils/Title";
 
 const Sales = ({ ifExists, endpoint: { title, items } }) => {
     return (
